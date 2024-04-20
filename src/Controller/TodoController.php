574e711b3d0e9584ccf91500b7376ed510e77ceb
Kerolous Amged <Kokp.amged@gmail.com>
2024-04-20 10:15:48 +0200

    refactor: removed BlogController

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -6,14 +6,61 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-class TodoController extends AbstractController
-{
-    #[Route('/todo', name: 'app_todo')]
-    public function index(): JsonResponse
+
+#[Route('/blogs')]
+class TodoController extends AbstractController{
+
+const POSTS = [
+    [
+        'id' => 1,
+        'slug' => 'hello-world',
+        'title' => 'Hello World',
+        'published' => true,
+    ],
+    [
+        'id' => 2,
+        'slug' => 'another-post',
+        'title' => 'This is another post',
+        'published' => false,
+    ],
+    [
+        'id' => 3,
+        'slug' => 'last-post',
+        'title' => 'This is the last post',
+        'published' => true,
+    ],  
+];
+
+
+    #[Route( name: 'app_blog_list' , path: '/' )]
+    public function index(): JsonResponse   {
+
+        $resourceIds = array_map( fn($post) => $this->generateUrl('app_blog_by_id' , ['id' => $post['id']] ) , SELF::POSTS );
+        $resourceSlugs = array_map( fn($post) => $this->generateUrl('app_blog_by_slug' , ['slug' => $post['slug']] ), SELF::POSTS );
+        
+        return $this->json([
+            'message' => 'Welcome to your new controller! he5a',
+            'blogs_ids' => $resourceIds,
+            'blogs_slugs' => $resourceSlugs,
+        ]);
+    } 
+
+    #[Route('/{id}', name: 'app_blog_by_id')]
+    public function blogById(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TodoController.php',
+            'path' => 'src/Controller/BlogController.php',
         ]);
     }
+
+    #[Route('/{slug}', name:'app_blog_by_slug')]
+    public function blogBySlug(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/BlogController.php',
+        ]);
+    }
+
 }
